267dd252b40e0d281d709e6e8219119754196d44
Ana Laura Lopes <131914458+AnaLLopes@users.noreply.github.com>
2025-06-20 22:52:00 +0000

    update

---
+++ imposto-service/src/main/java/com/bcopstein/imposto/ImpostoServiceApplication.java
@@ -0,0 +1,11 @@
+package com.bcopstein.imposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImpostoServiceApplication.class, args);
+    }
+}
